8ae84b7f77a870905a72cffc5a160e13add9c079
Damrongsak Naparat <neo@Damrongsaks-MacBook-Pro.local>
2021-07-11 09:11:29 +0700

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+const weatherform = document.querySelector('form')
+const search = document.querySelector('input')
+const message1 = document.querySelector('#message-1')
+const message2 = document.querySelector('#message-2')
+
+
+weatherform.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+
+    url = '/weather?address=' + location
+    message1.textContent = 'Loading...'
+    message2.textContent = ''
+
+    fetch(url).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                message1.textContent = data.error
+            } else {
+                console.log(data)
+                message1.textContent = data.location
+                message2.textContent = data.forecast.current + ' and it feels like ' + data.forecast.feelslike
+            }
+        })
+    })
+
+    console.log(location)
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('postman-request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=7a232548618291883a5351cd75c21190&query=' + encodeURIComponent(latitude) +
+    ',' + encodeURIComponent(longitude) + '&unit=m'
+
+    request({url, json: true}, (error, { body }) => {
+        if (error) {
+            callback('Connection Fail', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, {
+                current: body.current.weather_descriptions[0],
+                feelslike: body.current.feelslike
+            })
+        }
+    })
+}
+
+module.exports = forecast
